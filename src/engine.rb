0602b4d9c40bbe525d8d83e18f6f65ecb99c94d1
ice1000 <ice1000kotlin@foxmail.com>
2016-10-29 17:43:24 +0800

    commit via push.cmd

--- src/engine.rb
+++ src/engine.rb
@@ -1,2 +1,13 @@
-module Frice
+# Frice engine base class
+
+class Frice
+  def initialize()
+    @objs = []
+    @texts = []
+    @timers = []
+  end
+  def _on_init()
+  end
+  def _on_refresh()
+  end
 end
